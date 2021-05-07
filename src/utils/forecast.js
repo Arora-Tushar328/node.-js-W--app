a3c171b8b2aa0f1fee0f0fd7bc77593ff09c88cd
tushar arora <tushararora334@gmail.com>
2021-05-07 15:41:20 +0530

    Add New style

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined,body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out." +"Pressure is :"+body.current.pressure +". Humidity Is " +body.current.humidity+ "%"+ " . UV index is" + body.current.uv_index)
+            callback(undefined,body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out." +"Pressure is :"+body.current.pressure +". Humidity Is " +body.current.humidity+ "%"+ ". UV index is " + body.current.uv_index)
         }
     })
 }
