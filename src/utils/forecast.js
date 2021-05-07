bbdab510ffedef89c88587090e65a0de32766467
tushar arora <tushararora334@gmail.com>
2021-05-07 15:37:28 +0530

    Add New style

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,6 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            console.log(body.current.location)
             callback(undefined,body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out." +"Pressure is :"+body.current.pressure +" Humidity Is " +body.current.humidity+ "%"+ "UV index is" + body.current.uv_index)
         }
     })
